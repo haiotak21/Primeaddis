dbeac7ba48f8cf5c229a71962fd5227a00171dc5
haile takele <147279743+haiotak21@users.noreply.github.com>
2026-02-26 09:48:04 +0300

    Fix import path for Property model in route.ts

--- app/api/properties/site-visit/route.ts
+++ app/api/properties/site-visit/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import connectDB from "@/lib/database";
-import Property from "../../../models/Property"
+import Property from "../../../../models/Property";
 import SiteVisitRequest from "@/models/SiteVisitRequest";
 import nodemailer from "nodemailer";
 
